36467b7949f638b0ad6b37f8f83e110c4a5370af
Artem <artem.k.sergienko@gmail.com>
2022-01-17 16:57:56 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary:{
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
